8a7d74dc019ae0b78ddee63ede796801197f93df
hhzheng <zhenghuanhuan@zhejianglab.com>
2024-04-25 09:16:26 +0800

    begin to adjust download title and href in one csv file by each group url.

--- metweb_scraper.py
+++ metweb_scraper.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import scrapy
 from scrapy.crawler import Crawler
 from scrapy.settings import Settings
@@ -18,15 +20,20 @@ class met_web(scrapy.Spider):
 
     weather={}
     def start_requests(self):
-        start_url=['https://www.matweb.com/Search/MaterialGroupSearch.aspx?GroupID=178']    # ASTM Steel
+
         #start_url=['http://www.matweb.com/search/QuickText.aspx?SearchText=AA7075']
         #start_url = ['http://www.matweb.com/search/QuickText.aspx?SearchText=AA5083']
         #start_url = ['http://www.matweb.com/search/QuickText.aspx?SearchText=AA7150']
         #start_url =['http://www.matweb.com/search/QuickText.aspx?SearchText=AA1235']
+        start_url=['http://www.matweb.com/search/QuickText.aspx?SearchText=AA2618']     # this is for test not actual url
         for url in start_url:
             yield scrapy.Request(url=url, callback=self.parse)
 
+# # todo debug descriptor need to locate the head_ck
 
+#         material_url = ['http://www.matweb.com/search/DataSheet.aspx?MatGUID=e5e92a1ae7f24e1b918bf4e65dbc7e52']
+#         for url in material_url:
+#             yield scrapy.Request(url=url, callback=self.parse_2)
 
 
     #'//div[@id='details']/div/div[@class='day muted']/div/div/h4'
@@ -56,7 +63,6 @@ class met_web(scrapy.Spider):
 
                 for url in start_url:
                     yield scrapy.Request(url=url, callback=self.parse_2)
-                    break
             else:
                 print('empty cell', composition_link)
 
@@ -68,7 +74,7 @@ class met_web(scrapy.Spider):
         title=response.xpath("//table[@class='tabledataformat t_ableborder tableloose altrow']/tr/th/text()").extract_first()
         title = title.strip().replace('/','-')
 
-        print('\n title is ',title)
+        print('\ntitle is :',title)
         folder_path = os.getcwd()
         folder_path=folder_path+'/'+title
         if not os.path.exists(folder_path):
@@ -79,8 +85,44 @@ class met_web(scrapy.Spider):
         csv_file = folder_path + title + '.csv'
         if not os.path.exists(csv_file):
             detail_file.to_csv(csv_file,sep=',', encoding='utf-8',index=False)
+        ####################Physical property header_end ########################################################
+        d = {'col1': ['URL'], 'col2': [url], 'col3': ['Title'], 'col4': [title]}
 
+        df = pd.DataFrame(data=d)
+        table_title = ''
+        table_title_reminder = True
+        for resp in response.xpath(
+                "//table[@class='tabledataformat']/tr"):  # //div[@id='feed-tabs']#//div[@id='details']/div/div[@class='day muted']
+            header_ck = resp.xpath("tr[2]/th[1]/text()").extract_first()
+            #  //*[@id="ctl00_ContentMain_ucDataSheet1_pnlMaterialData"]/table[2]/tbody/tr[2]/th[1]
+            print('header_contain : ', header_ck)
 
+            if header_ck:
+
+                if table_title_reminder == True:
+                    table_title = header_ck
+                    table_title_reminder = False
+
+                physical_property = resp.xpath("th[1]/text()").extract_first()
+                metrix = resp.xpath("th[2]/text()").extract_first()
+                english = resp.xpath("th[3]/text()").extract_first()
+                comments = resp.xpath("th[4]/text()").extract_first()
+                pprint(physical_property, metrix, english, comments)
+                # if (metrix != 'None' and english != 'None'):
+                #     print('\n physical property is ',physical_property,metrix,english,comments)
+                #     d = {'col1': physical_property, 'col2': metrix, 'col3': english, 'col4': comments}
+                #
+                #     df = df.append(d, ignore_index=True)
+
+            else:
+                print('some thing unexpected happened')
+
+        print('\n ############### Dataframe is######################')
+        file_name=title+".csv"
+        print(df)
+        file_name = folder_path+table_title+".csv"
+        df.drop(df.index[0],inplace=True)
+        df.to_csv(file_name, sep=',', header=False,index=False)
 
 if __name__ == '__main__':
 
