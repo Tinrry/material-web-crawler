39f685cf15171a21744882f90c9fe67be2318842
hhzheng <zhenghuanhuan@zhejianglab.com>
2024-04-26 11:21:26 +0800

    add read pandas that can read get_link files and save the downloaded  html as the title name

--- matweb.py
+++ matweb.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-def get_html(save_path, paper_url):
+def get_html(save_file, paper_url):
     # paper_url = "https://www.matweb.com/search/DataSheet.aspx?MatGUID=e5e92a1ae7f24e1b918bf4e65dbc7e52"
 
     chrome_options = Options()
@@ -29,15 +30,30 @@ def get_html(save_path, paper_url):
         )
 
         html = element.get_attribute("innerHTML")
-        file_name = os.path.join(save_path, paper_url[-5:] + '.html')
-        with open(file_name, 'w') as file:
+        with open(save_file, 'w') as file:
             file.write(html)
     finally:
         driver.quit()
 
+
+def read_file(abs_file):
+    # todo read csv file
+    data_frame = pd.read_csv(abs_file, header=0)
+    return data_frame
+
+
 if __name__ == '__main__':
     paper_url = "https://www.matweb.com/search/DataSheet.aspx?MatGUID=e5e92a1ae7f24e1b918bf4e65dbc7e52"
+    # TODO get link from get_link/csv files and get html
+    get_link = 'get_link'
+
     save_path = "get_html"
     if not os.path.exists(save_path):
         os.makedirs(save_path)
-    get_html(save_path, paper_url)
+
+    for _, _, files in os.walk(get_link):
+        for file in files:
+            data_frame = read_file(os.path.join(get_link, file))
+            for title, paper_url in zip(data_frame['title'], data_frame['href']):
+                save_file = os.path.join(save_path, title + '.html')
+                get_html(save_file, paper_url)
