0dd46b9805145b7af7a9b8dcecd04936bb3492cd
hhzheng <zhenghuanhuan@zhejianglab.com>
2024-08-14 14:39:44 +0800

    add sort formula.

--- formula_add_1.sh
+++ formula_add_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 输入字符串
-input_file="compositions.txt"
+input_file="compositions-2.txt"
 while IFS= read -r line; do
     # 提取链接
     link=$(echo "$line" | awk -F':' '{print $1 ":" $2}')
@@ -13,9 +13,9 @@ while IFS= read -r line; do
     elements=$(echo "$formula" | grep -oE '[A-Z][a-z]*[0-9]*' | sed 's/\([A-Z][a-z]*\)\([0-9]*\)/\1 \2/' | awk '{if ($2 == "") $2 = 1; print $1 $2}')
 
     # 将结果合并成一行，并输出结果
-    elements=$(echo "$elements" | tr -d '\n')
+    elements=$(echo "$elements" | sort | tr -d '\n')
 
     # 将结果写入文件
-    echo $link":"$elements >> compositions-CIF.txt
+    echo $link":"$elements
 
 done < "$input_file"
