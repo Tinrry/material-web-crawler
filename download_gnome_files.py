41b7f62f5fc6a979905469ac85dc1a6ec6cbe0b7
hhzheng <zhenghuanhuan@zhejianglab.com>
2024-08-16 17:44:48 +0800

    download list of vasp zip.

---
+++ download_gnome_files.py
@@ -0,0 +1,50 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# driver = webdriver.Firefox()
+
+def login(driver):
+  driver.get("https://profile.materialsproject.org/connect/portier")
+  email = input("Wait till the login page is ready and then input the email address: ")
+  elem = driver.find_element(By.NAME, "login_hint")
+  elem.send_keys(email)
+  elem.send_keys(Keys.RETURN)
+  code = input("Wait till the email is recieved and input the code in the email: ")
+  elem = driver.find_element(By.NAME, "code")
+  elem.send_keys(code)
+  elem.send_keys(Keys.RETURN)
+  time.sleep(20) # Wait
+
+
+def download(driver, url):
+  try:
+    driver.get(url)
+    # Tried WebDriverWait+EC but this hardcoded wait seems to be more consistent smh.
+    time.sleep(10) # Wait for webpage to load
+    elements = driver.find_elements(By.CLASS_NAME, 'mpc-button-bar')
+    b = elements[0].find_elements(By.CLASS_NAME, 'dropdown-trigger')[-1].find_elements(By.XPATH, 'button')[0]
+    b.send_keys(Keys.ENTER)
+    time.sleep(2) # Wait for webpage to response
+    elements[0].find_elements(By.CLASS_NAME, "dropdown-content")[0].find_elements(By.XPATH, "div")[-1].click()
+    time.sleep(2) # Wait for webpage to response
+  except Exception as e:
+    print(f"Error downloading {url}: {e}")
+    
+  return
+
+if __name__ == "__main__":
+  url_list = ["https://next-gen.materialsproject.org/materials/gnome/mp-3199100", 
+              "https://next-gen.materialsproject.org/materials/gnome/mp-3170010", 
+              "https://next-gen.materialsproject.org/materials/gnome/mp-3170054", 
+              "https://next-gen.materialsproject.org/materials/gnome/mp-3199060"]
+
+  driver = webdriver.Firefox()
+  login(driver)
+  for url in url_list:
+    download(driver, url)
+  driver.quit()
