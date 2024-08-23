42b54f0ea19c3fe14d94ef88c5ad07cb4c926768
hhzheng <zhenghuanhuan@zhejianglab.com>
2024-08-23 15:42:25 +0800

    ok, can done. the results are save in chemistry_href.txt

--- download_chemistry.py
+++ download_chemistry.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+from download_gnome_files import login
+
 
 # get periodic table
 periodic_table = [
@@ -16,29 +18,51 @@ periodic_table = [
     'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og'
 ]
 
-base_href = 'https://next-gen.materialsproject.org/materials?formula=P'
-mp_href = 'https://next-gen.materialsproject.org/materials/mp-568348?formula=P'
+base_href = 'https://next-gen.materialsproject.org/materials?formula=Bi' # 2 个链接
+mp_href = 'https://next-gen.materialsproject.org/materials/mp-568348?formula=P'   # 1 个链接  
 
-# TODO 同一个元素，可能有很多个能量为0的结构
+
 def find_zero_energy_href(driver, chemistry):
     base_href = 'https://next-gen.materialsproject.org/materials?formula=' + chemistry
 
-    mp_href = None
+    mp_href = []
     driver.get(base_href)
     time.sleep(10)
-    elements = driver.find_elements(By.ID, 'row-0')
-    energy = elements[0].find_elements(By.XPATH, 'div')[6].text
-    print(f'energy {energy}')
-    if int(energy) == 0:
-        # extended_href = elements[0].find_elements(By.XPATH, 'div')[1].get_attribute('a href')
-        extended_href = elements[0].find_elements(By.XPATH, 'div')[1].find_elements(By.XPATH, 'a')[0].get_attribute('href')
-        print(f'extended_href {extended_href}')
-    return extended_href
 
-from download_gnome_files import login
+    row = -1
+    while True:
+        row += 1
+        try:
+            elements = driver.find_elements(By.ID, f'row-{row}')
+        except Exception as e:
+            print(f'Error: {e}')
+            return mp_href
+        
+        energy = elements[0].find_elements(By.XPATH, 'div')[6].text
+        print(f'energy {energy}')
+        if energy == '0':
+            # extended_href = elements[0].find_elements(By.XPATH, 'div')[1].get_attribute('a href')
+            extended_href = elements[0].find_elements(By.XPATH, 'div')[1].find_elements(By.XPATH, 'a')[0].get_attribute('href')
+            print(f'extended_href {extended_href}')
+            mp_href.append(extended_href)
+        else:
+            break
+    return mp_href
+
+import os
+
 
 if __name__ == "__main__":
+
     driver = webdriver.Firefox()
     login(driver)
-    mp_href = find_zero_energy_href(driver, 'P')
+
+    if os.path.exists('chemistry_href.txt'):
+        os.remove('chemistry_href.txt')
+    for chemistry in periodic_table[:2]:
+        mp_href = find_zero_energy_href(driver, chemistry)
+        # write mp_href to txt file
+        with open('chemistry_href.txt', 'a') as file:
+            for href in mp_href:
+                file.write(f'{href}\n')
     driver.close()
